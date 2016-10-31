fe51c95b1aba3b957bb3bc7e8c25705cba18afcb
Sayontan Chowdhury <sayonchy@gmail.com>
2016-10-31 20:56:48 +0600

    Minor Changes to requesthandlerfactory

--- OverDrive/src/Net/OverdriveRequestHandlerFactory.cpp
+++ OverDrive/src/Net/OverdriveRequestHandlerFactory.cpp
@@ -1,6 +1,6 @@
 #include "OverdriveRequestHandlerFactory.hpp"
 #include "Handlers.hpp"
-#include <cstring>
+#include <string>
 
 namespace Overdrive {
 namespace Net {
@@ -9,10 +9,16 @@ namespace Net {
 		//if (request.getURI() == "/")
 		if (request.getURI() == "/hello") return new myRequestHandler;
 		//change this myRequestHandler when u remove testTransfer.hpp
-		else if (strstr(request.getURI().c_str(), "/img/")) {
+		/*else if (strstr(request.getURI().c_str(), "/img/")) {
 			return new generalResourceHandler();
 		}
 		else if (strstr(request.getURI().c_str(), "/css/")) {
+			return new CSSHandler();
+		}*/
+		else if (request.getURI().find("/img/") != std::string::npos) {
+			return new generalResourceHandler();
+		}
+		else if (request.getURI().find("/css/") != std::string::npos) {
 			return new CSSHandler();
 		}
 		else if (request.getURI() == "/test") return new FormRequestHandler;
